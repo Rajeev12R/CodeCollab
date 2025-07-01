22757dbb61186be03a3e6b648e20bcd072ab9c4c
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-07-01 15:06:47 +0530

    feat: added Header/Navbar component

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-center justify-between p-4 bg-foreground text-background'>
+        <div className='text-2xl font-bold font-sans'>CodeCollab</div>
+
+    </div>
+  )
+}
+
+export default Header
